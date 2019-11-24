d6be1ca8014c40671979d62a92c300cbd46c1e31
claudionc <claudionc@ciandt.com>
2019-11-24 11:20:02 -0300

    feet: 02 - 03 - Navegando entre destinos

--- app/src/main/java/br/com/alura/aluraesporte/ui/activity/ProdutosActivity.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/activity/ProdutosActivity.kt
@@ -10,6 +10,7 @@ import br.com.alura.aluraesporte.ui.fragment.ListaProdutosFragment
 import br.com.alura.aluraesporte.ui.fragment.PagamentoFragment
 import org.koin.android.ext.android.inject
 
+//TODO: Avaliar possível remoção
 private const val COMPRA_REALIZADA = "Compra realizada"
 
 class ProdutosActivity : AppCompatActivity() {
@@ -17,47 +18,49 @@ class ProdutosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.produtos_activity)
-        if (savedInstanceState == null) {
+        //TODO: Remover código
+//        if (savedInstanceState == null) {
 //            val produtosFragment: ListaProdutosFragment by inject()
 //            transacaoFragment {
 //                replace(R.id.container, produtosFragment)
 //            }
-        }
+//        }
     }
 
-    override fun onAttachFragment(fragment: Fragment?) {
-        super.onAttachFragment(fragment)
-        when (fragment) {
-            is ListaProdutosFragment -> {
-                fragment.quandoProdutoSelecionado = { produtoSelecionado ->
-                    val detalhesProdutoFragment: DetalhesProdutoFragment by inject()
-                    val argumentos = Bundle()
-                    argumentos.putLong(CHAVE_PRODUTO_ID, produtoSelecionado.id)
-                    detalhesProdutoFragment.arguments = argumentos
-                    transacaoFragment {
-                        addToBackStack(null)
-                        replace(R.id.container, detalhesProdutoFragment)
-                    }
-                }
-            }
-            is DetalhesProdutoFragment -> {
-                fragment.quandoProdutoComprado = { produtoComprado ->
-                    val pagamentoFragment: PagamentoFragment by inject()
-                    val dado = Bundle()
-                    dado.putLong(CHAVE_PRODUTO_ID, produtoComprado.id)
-                    pagamentoFragment.arguments = dado
-                    transacaoFragment {
-                        addToBackStack(null)
-                        replace(R.id.container, pagamentoFragment)
-                    }
-                }
-            }
-            is PagamentoFragment -> {
-                fragment.quandoPagamentoRealizado = {
-                    Toast.makeText(this, COMPRA_REALIZADA, Toast.LENGTH_LONG).show()
-                }
-            }
-        }
-    }
+    //TODO: Remover código
+//    override fun onAttachFragment(fragment: Fragment?) {
+//        super.onAttachFragment(fragment)
+//        when (fragment) {
+//            is ListaProdutosFragment -> {
+//                fragment.quandoProdutoSelecionado = { produtoSelecionado ->
+//                    val detalhesProdutoFragment: DetalhesProdutoFragment by inject()
+//                    val argumentos = Bundle()
+//                    argumentos.putLong(CHAVE_PRODUTO_ID, produtoSelecionado.id)
+//                    detalhesProdutoFragment.arguments = argumentos
+//                    transacaoFragment {
+//                        addToBackStack(null)
+//                        replace(R.id.container, detalhesProdutoFragment)
+//                    }
+//                }
+//            }
+//            is DetalhesProdutoFragment -> {
+//                fragment.quandoProdutoComprado = { produtoComprado ->
+//                    val pagamentoFragment: PagamentoFragment by inject()
+//                    val dado = Bundle()
+//                    dado.putLong(CHAVE_PRODUTO_ID, produtoComprado.id)
+//                    pagamentoFragment.arguments = dado
+//                    transacaoFragment {
+//                        addToBackStack(null)
+//                        replace(R.id.container, pagamentoFragment)
+//                    }
+//                }
+//            }
+//            is PagamentoFragment -> {
+//                fragment.quandoPagamentoRealizado = {
+//                    Toast.makeText(this, COMPRA_REALIZADA, Toast.LENGTH_LONG).show()
+//                }
+//            }
+//        }
+//    }
 
 }
